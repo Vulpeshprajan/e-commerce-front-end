46a386712d09782766086d2592e6fc341a61cb71
Vulpeshprajan <vulpeashprajan@gmail.com>
2021-10-14 22:53:46 +1100

    initail commit

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# e-commerce-front-end
+# Ecommerce admin cms
+
+This project is for the admin panel of the ecommerce site.
+
+## How to run
+
+- Clone the project
+- Run `npm install`
---
+++ src/App.js
@@ -0,0 +1,13 @@
+import { Button } from "react-bootstrap";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      Starting admin cms
+     
+    </div>
+  );
+}
+
+export default App;
